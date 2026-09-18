67f8827a08d605ec62a3af9e752f13c9b39c8958
Felp <wtffelp@gmail.com>
2026-09-18 13:28:27 -0300

    feat: create base backend structure

---
+++ server/apinest/src/main/java/com/apinest/App.java
@@ -0,0 +1,13 @@
+package com.apinest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
