a36de85bcf042717ef7bbb9cbc74ca80f59c93a1
Felp <wtffelp@gmail.com>
2026-09-21 16:36:11 -0300

    feat: set up Javalin app entrypoint with root route

--- server/apinest/src/main/java/com/apinest/App.java
+++ server/apinest/src/main/java/com/apinest/App.java
@@ -1,13 +1,11 @@
 package com.apinest;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import io.javalin.Javalin;
+
+public class App {
+  public static void main(String[] args) {
+    var app = Javalin.create(config -> {
+      config.routes.get("/", ctx -> ctx.result());
+    }).start(7070);
+  }
 }
